01c0840baf0300d010b178652cf4c6fd77cca50c
Fran <franeldaiavlo@gmail.com>
2026-04-03 16:45:25 -0600

    Updated LogIn Front End to add a Transition

--- frontend/src/Components/LoginSignup/LoginSignup.jsx
+++ frontend/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,41 +1,72 @@
-
 import React, { useState } from "react";
-import "./LoginSignup.css"
-
+import "./LoginSignup.css";
 import user_icon from "../Assets/person.png"
 import email_icon from "../Assets/email.png"
 import password_icon from "../Assets/password.png"
 
 const LoginSignup = () => {
-    const [action, setAction] = useState("Sign up");
-    return (
-        <div className='container'>
-            <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
-            </div>
-            <div className='inputs'>
-                {action === "Login" ? <div></div> : <div className="input">
-                    <img src={user_icon} alt="" className="" />
-                    <input type="text" placeholder='Name' className="" />
-                </div>}
-                <div className="input">
-                    <img src={email_icon} alt="" className="" />
-                    <input type="email" placeholder='Email' className="" />
-                </div>
-                <div className="input">
-                    <img src={password_icon} alt="" className="" />
-                    <input type="password" placeholder='Password' className="" />
-                </div>
-            </div>
-            {action === "Sign up" ? <div></div> : <div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+    const [isRightPanelActive, setIsRightPanelActive] = useState(false);
 
-            <div className="submit-container">
-                <div className={action === "Sign up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
-                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign up") }}>Sign up</div>
+    const handleSignUpClick = () => {
+        setIsRightPanelActive(true);
+    };
+
+    const handleSignInClick = () => {
+        setIsRightPanelActive(false);
+    };
+
+    return (
+        <div className={`container ${isRightPanelActive ? "right-panel-active" : ""}`} id="container">
+            <div className="form-container sign-up-container">
+                <form action="#" onSubmit={(e) => e.preventDefault()}>
+                    <h1>Create Account</h1>
+                    <span>use your email for registration</span>
+                    <div className="input-group">
+                        <img src={user_icon} alt="user" />
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div className="input-group">
+                        <img src={email_icon} alt="email" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input-group">
+                        <img src={password_icon} alt="password" />
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <button>Sign Up</button>
+                </form>
+            </div>
+            <div className="form-container sign-in-container">
+                <form action="#" onSubmit={(e) => e.preventDefault()}>
+                    <h1>Sign In</h1>
+                    <div className="input-group">
+                        <img src={email_icon} alt="email" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="input-group">
+                        <img src={password_icon} alt="password" />
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <a href="#">Forgot your password?</a>
+                    <button>Sign In</button>
+                </form>
+            </div>
+            <div className="overlay-container">
+                <div className="overlay">
+                    <div className="overlay-panel overlay-left">
+                        <h1>Welcome Back!</h1>
+                        <p>To keep connected with us please login with your personal info</p>
+                        <button className="ghost" id="signIn" onClick={handleSignInClick}>Sign In</button>
+                    </div>
+                    <div className="overlay-panel overlay-right">
+                        <h1>Hello, Friend!</h1>
+                        <p>Enter your personal details and start journey with us</p>
+                        <button className="ghost" id="signUp" onClick={handleSignUpClick}>Sign Up</button>
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default LoginSignup
+export default LoginSignup;
